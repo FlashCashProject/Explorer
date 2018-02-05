01f2483bd4737f011a351cf02a6326a2fad570ef
FlashCashProject <36159563+FlashCashProject@users.noreply.github.com>
2018-02-05 15:15:32 +0200

    Initial commit

---
+++ config.js
@@ -0,0 +1,18 @@
+var api = 'http://69.172.229.230:80';
+var blockTargetInterval = 180;
+var coinUnits = 10000000000;
+var symbol = 'FLC';
+var refreshDelay = 30000;
+// pools stats by MainCoins
+var networkStat = {
+    "krb": [
+	["krb.dreampool.info", "https://krb.dreampool.info/api"]
+		
+    ]
+};
+
+var networkStat2 = {
+    "krb": [
+		["karbowanec.hashvault.pro", "https://karbowanec.hashvault.pro/api"]
+		]
+};
---
+++ q/hashrate/index.php
@@ -0,0 +1,14 @@
+<?php 
+header('Access-Control-Allow-Origin: *');
+$ch = curl_init();
+curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+curl_setopt($ch, CURLOPT_URL, 'http://69.172.229.230:80/getinfo');
+$result = curl_exec($ch);
+$obj = json_decode($result, TRUE);
+curl_close($ch);
+//print_r($obj);
+$difficulty = $obj['difficulty'];
+$hashrate = round($difficulty / 240);
+print_r($hashrate);
+?>
